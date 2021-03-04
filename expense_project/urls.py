feed8416e8ab6c148bcee22b7d7b04336e68365c
rishank-shah <shahrishank@gmail.com>
2021-03-04 11:03:08 +0530

    html refactoring and index page added

--- expense_project/urls.py
+++ expense_project/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from django.conf import settings
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html")),
     path('auth/',include('auth_app.urls')),
     path('expense/',include('expense_app.urls')),
     path('user/',include('user_profile.urls')),
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
